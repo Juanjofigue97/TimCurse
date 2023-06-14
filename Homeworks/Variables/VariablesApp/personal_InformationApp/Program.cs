a9ee5160b72dfbd51afeaaa1666812ad2efbbb78
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-14 09:38:07 -0500

    Personal Information App

---
+++ Homeworks/Variables/VariablesApp/personal_InformationApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Console.WriteLine("This application will store personal information of a person.");
+Console.WriteLine("This application is made by Juan Jose Figueroa.");
+Console.WriteLine("_________________________________________________________");
+Console.WriteLine();
+
+// Personal Information
+
+string fullName = "Juan Jose Figueroa";
+
+int age = 27;
+
+bool isAlive = true;
+
+string phoneNumber = "(+57) 3216972161";
+
+// Show personal infomation
+
+Console.WriteLine($"\tPersonal Information: ");
+Console.WriteLine($"-\tName: {fullName}");
+Console.WriteLine($"-\tAge: {age}");
+Console.WriteLine($"-\tAlive: {isAlive}");
+Console.WriteLine($"-\tPhone Number: {phoneNumber}");
+
+// Finish the app
+Console.WriteLine();
+Console.WriteLine("_________________________________________________________");
+Console.WriteLine("Thanks for using my app");
+Console.WriteLine("See you Later!");
+
+Console.ReadLine();
