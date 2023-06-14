cf33b70a5640cdfe197b437996fae4fff097e0f3
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-14 17:43:10 -0500

    Practice Proyects

---
+++ OwnProjects/07_Boolean/VariablesBooleanApp/TruthTablesApp/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+Console.WriteLine("This is a program to determine truth tables for operations such as: \n- AND \n- OR \n- NOT \n- XOR.");
+Console.WriteLine("This application was made by Juan Jose Figueroa.");
+Console.WriteLine("---------------------------------------------------");
+Console.WriteLine();
+
+bool firstInput = true;
+bool secondInput = false;
+
+Console.WriteLine(@"  Truth tables");
+Console.WriteLine(@"  1.Table for Negation");
+Console.WriteLine($@"    P: {firstInput}");
+Console.WriteLine($@"   ¬P: {!firstInput}");
+Console.WriteLine();
+Console.WriteLine(@"  2.Table for OR");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@"    P   |   Q   | Q or P");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {firstInput}  | {firstInput || firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {secondInput} | {firstInput || secondInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {firstInput}  | {secondInput || firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {secondInput} | {secondInput || secondInput}");
+Console.WriteLine();
+Console.WriteLine(@"  2.Table for AND");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@"    P   |   Q   | Q and P");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {firstInput}  | {firstInput && firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {secondInput} | {firstInput && secondInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {firstInput}  | {secondInput && firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {secondInput} | {secondInput && secondInput}");
+Console.WriteLine();
+Console.WriteLine(@"  3.Table for XOR");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@"    P   |   Q   | XOR ");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {firstInput}  | {!firstInput || !firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {firstInput}  | {secondInput} | {!firstInput || !secondInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {firstInput}  | {!secondInput || !firstInput}");
+Console.WriteLine(@"  -----------------------");
+Console.WriteLine(@$"  {secondInput} | {secondInput} | {secondInput || secondInput}");
+
+
---
+++ OwnProjects/08_Null/NullKeyWordApp/variableNull/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("In this application, we will interact with the reserved word 'null'.");
+Console.WriteLine("This program is built by Juan Jose Figueroa.");
+Console.WriteLine("-----------------------------------------------------------");
+Console.WriteLine();
+
+int? number = null;
+
+Console.WriteLine(number.HasValue ? number.Value.ToString() : "This variable is null");
+Console.WriteLine(number?.ToString());
+
+double? averageWeight = null;
+
+decimal? totalWeightEarth = null;
+
+string? fullname = null;
+Console.WriteLine("-----------------------------------------------------------");
+Console.WriteLine(averageWeight);
+Console.WriteLine(totalWeightEarth);
+Console.WriteLine(fullname);
