c74ac400638bf26dbee70ac3751e09a0e60ab28a
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 17:57:56 -0500

    DateOnly

--- OwnProjects/10_DateOnly/DateOnlyApps/FirstDateOnlyApp/Program.cs
+++ OwnProjects/10_DateOnly/DateOnlyApps/FirstDateOnlyApp/Program.cs
@@ -1,6 +1,17 @@
-﻿Console.WriteLine("I'm going to create DateOnly");
+﻿
 
-DateOnly dateOnly = new DateOnly();
+Console.WriteLine("DateOnly");
+var theDate = new DateOnly(2015, 10, 21);
 
-Console.WriteLine(dateOnly);
+var nextDay = theDate.AddDays(1);
+var previousDay = theDate.AddDays(-1);
+var decadeLater = theDate.AddYears(10);
+var lastMonth = theDate.AddMonths(-1);
 
+
+
+Console.WriteLine($"Date: {theDate}");
+Console.WriteLine($" Next day: {nextDay}");
+Console.WriteLine($" Previous day: {previousDay}");
+Console.WriteLine($" Decade later: {decadeLater}");
+Console.WriteLine($" Last month: {lastMonth}");
---
+++ OwnProjects/10_DateOnly/DateOnlyApps/ParseDate/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Globalization;
+
+DateOnly dateOnly = DateOnly.Parse("11-13-2012",CultureInfo.InvariantCulture);
+
+Console.WriteLine(dateOnly);
+
+CultureInfo Colombia = new CultureInfo("es-CO");
+
+DateOnly Date2 = DateOnly.ParseExact("19-08-29","yy-MM-dd", Colombia);
+
+Console.WriteLine($"{Date2.DayOfWeek}");
+
+CultureInfo enUs = new CultureInfo("es-CO");
+
+foreach (string dayName in enUs.DateTimeFormat.DayNames)
+    Console.WriteLine(dayName);
+Console.WriteLine("Today is: " + enUs.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek));
