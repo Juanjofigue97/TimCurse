29dbe3c8365245dca5c6bd0df253d699892fea71
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 16:38:09 -0500

    Full Date

---
+++ OwnProjects/09_DataTime/DataTimeApps/DayPropertiesApp/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("This application displays the Day properties (year, month, day, hour, minute, second, millisecond etc.)");
+Console.WriteLine("This program is built by Juan Jose Figueroa");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
+
+var DayProperties = new DateTime(2016, 8, 16, 3, 57, 32, 11);
+
+Console.WriteLine($"year = {DayProperties.Year}");
+Console.WriteLine($"month = {DayProperties.Month}");
+Console.WriteLine($"day = {DayProperties.Day}");
+Console.WriteLine($"hour = {DayProperties.Hour}");
+Console.WriteLine($"minute = {DayProperties.Minute}");
+Console.WriteLine($"second = {DayProperties.Second}");
+Console.WriteLine($"millisecond = {DayProperties.Millisecond}");
