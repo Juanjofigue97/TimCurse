016ce3dc256d6f1bde4b968fc3dc6a4822c4912d
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 09:04:45 -0500

    Using Date Time Constructor

---
+++ OwnProjects/09_DataTime/DataTimeApps/UsingDatimeConstructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+DateTime localDate = DateTime.Now;
+DateTime utcDate = DateTime.UtcNow;
+List<string> cultureNames = new()
+            {
+                "en-JM", "en-NZ", "fi-FI","de-CH", "nl-NL"
+            };
+
+foreach (var cultureName in cultureNames)
+{
+    var culture = new CultureInfo(cultureName);
+    Console.WriteLine("{0}:", culture.NativeName);
+    Console.WriteLine("   Local date and time: {0}, {1:G}",
+                      localDate.ToString(culture), localDate.Kind);
+    Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
+                      utcDate.ToString(culture), utcDate.Kind);
+
+}
+var TimeS = new TimeSpan().Ticks;
+Console.WriteLine(TimeS);
