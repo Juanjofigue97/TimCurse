c441473f41ca492b770e1e749828de1a4c6128f0
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 16:36:15 -0500

    Using the constructor of the class to create a date.

--- OwnProjects/09_DataTime/DataTimeApps/UsingDatimeConstructor/Program.cs
+++ OwnProjects/09_DataTime/DataTimeApps/UsingDatimeConstructor/Program.cs
@@ -1,21 +1,17 @@
-﻿using System.Globalization;
+﻿
 
-DateTime localDate = DateTime.Now;
-DateTime utcDate = DateTime.UtcNow;
-List<string> cultureNames = new()
-            {
-                "en-JM", "en-NZ", "fi-FI","de-CH", "nl-NL"
-            };
+Console.WriteLine("This application will demonstrate the different ways of writing a date with a Constructor");
+Console.WriteLine("This program is built by Juan Jose Figueroa");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
 
-foreach (var cultureName in cultureNames)
-{
-    var culture = new CultureInfo(cultureName);
-    Console.WriteLine("{0}:", culture.NativeName);
-    Console.WriteLine("   Local date and time: {0}, {1:G}",
-                      localDate.ToString(culture), localDate.Kind);
-    Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
-                      utcDate.ToString(culture), utcDate.Kind);
+DateTime FullDate = new DateTime(2016, 8, 6, 11, 49, 00);
+DateTime ShortDate = new DateTime(2016, 8, 6);
 
-}
-var TimeS = new TimeSpan().Ticks;
-Console.WriteLine(TimeS);
+Console.WriteLine($"Complete date: {FullDate}");
+Console.WriteLine($"Short date: {ShortDate:d}");
+//Console.WriteLine($"Short date: {FullDate.ToShortDateString()}");
+Console.WriteLine($"Display date using 24-hour clock format:");
+Console.WriteLine($"Complete date 24: {FullDate:dd/MM/yyyy HH:mm:ss}");
+Console.WriteLine($"Complete date 24: {FullDate:g}");
+Console.WriteLine($"Short date 24: {ShortDate:dd/MM/yyyy HH:mm}");
