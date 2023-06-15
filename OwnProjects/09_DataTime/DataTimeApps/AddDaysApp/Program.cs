1cdf048f0fc5201535d9a82d78d3e46bda2292d3
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 16:35:51 -0500

    Using the AddDays method

---
+++ OwnProjects/09_DataTime/DataTimeApps/AddDaysApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("This application calculate what day of the week is 40 days from now");
+Console.WriteLine("This program is built by Juan Jose Figueroa");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
+
+
+var DayOfWeek = new DateTime(2016, 8, 20, 16,18,17);
+
+var addDays = DayOfWeek.AddDays(40);
+
+Console.WriteLine($"{DayOfWeek:MM/dd/yyyy hh:mm:ss tt}");
+Console.WriteLine($"The day in 40 days will be {addDays.DayOfWeek}.");
