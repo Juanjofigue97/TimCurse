cc8e91b44f9d9fc2701ddc4d30ab3e5ba4f4864c
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 11:40:23 -0500

    Hello class Using Switch statement

---
+++ Homeworks/If_Switch_Statements/If_Switch_Statements/HelloClass_Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine("This application will greet you according to your name.");
+Console.WriteLine("This program is built by Juan Jose Figueroa.");
+
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Welcome to the C# classes!");
+Console.Write("What's your name: ");
+
+string? userName = Console.ReadLine();
+
+switch (userName?.ToLower())
+{
+    case "tim" or "timothy":
+        Console.WriteLine("Welcome Professor, how are you!");
+        break;
+    default:
+        Console.WriteLine("Hello student, how is your day going?");
+        break;
+}
+
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Thank you for using my application. See you soon!");
+Console.WriteLine();
+Console.ReadLine();
