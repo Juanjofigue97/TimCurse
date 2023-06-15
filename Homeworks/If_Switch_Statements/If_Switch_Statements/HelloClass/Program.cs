07ccf14dd37e971cd75ece67d1bca35ebb61d985
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 11:40:03 -0500

    HelloClass using if statement

---
+++ Homeworks/If_Switch_Statements/If_Switch_Statements/HelloClass/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.WriteLine("This application will greet you according to your name.");
+Console.WriteLine("This program is built by Juan Jose Figueroa.");
+
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Welcome to the C# classes!");
+Console.Write("What's your name: ");
+
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "tim" || userName.ToLower() == "timothy")
+{
+    Console.WriteLine("Welcome Professor, how are you!");
+}
+else
+{
+    Console.WriteLine("Hello student, how is your day going?");
+}
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+
+Console.WriteLine("Thank you for using my application. See you soon!");
+Console.WriteLine();
+Console.ReadLine();
