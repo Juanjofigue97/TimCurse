1049c5b127ee0c88a6d2a1d29f03399cdb03493e
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 16:37:51 -0500

    The local time on the computer and in different parts of the world

---
+++ OwnProjects/09_DataTime/DataTimeApps/LocalTime_CultureNames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Console.WriteLine("This application displays the time in local computer and others Conuntries");
+Console.WriteLine("This program is built by Juan Jose Figueroa");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
+
+
+//https://www.csharp-examples.net/culture-names/
+DateTime localDate = DateTime.Now;
+DateTime utcDate = DateTime.UtcNow; //Global Date
+List<string> cultureNames = new()
+            {
+                "en-IE", "en-ZA", "fr-CA","de-CH", "ro-RO","es-CO"
+            };
+
+foreach (var cultureName in cultureNames)
+{
+    var culture = new CultureInfo(cultureName);
+    Console.WriteLine("{0}:", culture.NativeName);
+    Console.WriteLine("   Local date and time: {0}, {1:G}",
+                      localDate.ToString(culture), localDate.Kind);
+    Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
+                      utcDate.ToString(culture), utcDate.Kind);
+
+}
