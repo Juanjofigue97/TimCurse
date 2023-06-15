f79b1d4f103c0a9f34a56605ca431e5acb9d0bfc
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 15:46:25 -0500

    Ask name multiple times until the user writes "Exit"

---
+++ Homeworks/Do_Loops/DoWhileLoopApp/OnlyWhileLoopAskName/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Console.WriteLine("This application is to greet all the students and professors who use it until you want to exit the program.");
+Console.WriteLine("This application was built by Juan Jose Figueroa.");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine();
+bool isExit = true;
+while (isExit)
+{
+    Console.Write("What is your name: ");
+    string firstName = Console.ReadLine();
+    if (firstName.ToUpper() == "TIM")
+    {
+        Console.WriteLine($"Welcome Professor {firstName}");
+    }
+    else
+    {
+        Console.WriteLine($"Welcome Student {firstName}");
+    }
+    Console.WriteLine("Do you want to exit the application? Type 'exit' to exit.");
+    string exit = Console.ReadLine();
+    if (exit.ToUpper() == "EXIT")
+    {
+        isExit = false;
+    }
+};
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Thank you for using this program, greetings");
