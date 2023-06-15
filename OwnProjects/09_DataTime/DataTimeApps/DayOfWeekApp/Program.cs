1ca87c0a01d45223af42a661da58df07e99433a9
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-15 16:36:38 -0500

    Using the DayOfWeek property.

---
+++ OwnProjects/09_DataTime/DataTimeApps/DayOfWeekApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("This application get the day of the week for a specified date");
+Console.WriteLine("This program is built by Juan Jose Figueroa");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
+
+
+var DayOfWeek = new DateTime(2016,11,7);
+
+Console.WriteLine($"The day of the week for {DayOfWeek:d/MM/yyyy} is {DayOfWeek.DayOfWeek}.");
