142ff2cb5540ef68a292955ad6e76d1da93859cc
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-17 10:57:22 -0500

    NullApps

---
+++ OwnProjects/08_Null/NullKeyWordApp/Badge/Badge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badge
+{
+    internal class Badge
+    {
+        public static string Print(int? id, string name, string? department)
+        {
+            string print = "";
+            print += id != null ? $"[{id}] - " : "";
+            print += $"{name} - ";
+            print += department == null ? "OWNER" : department.ToUpper();
+
+            return print;
+        }
+    }
+}
---
+++ OwnProjects/08_Null/NullKeyWordApp/Badge/BadgeComunnity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badge
+{
+    internal class BadgeComunnity
+    {
+        private const string DefaultDerpartment = "owner";
+        public static string Print(int? id, string name, string? department)
+        {
+            department = (department ?? DefaultDerpartment).ToUpper();
+            return id.HasValue switch
+            {
+                true => $"[{id}] - {name} - {department}",
+                false => $"{name} - {department}",
+            };
+        }
+    }
+}
