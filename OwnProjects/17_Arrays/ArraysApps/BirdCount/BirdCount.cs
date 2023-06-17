6fbafdb54ebbde886161ac45ee0635ece258fb5e
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-17 10:26:07 -0500

    Bird Count Array Project

---
+++ OwnProjects/17_Arrays/ArraysApps/BirdCount/BirdCount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdCount
+{
+    internal class BirdCount
+    {
+        private int[] birdsPerDay;
+
+        public BirdCount(int[] birdsPerDay)
+        {
+            this.birdsPerDay = birdsPerDay;
+        }
+
+        public static int[] LastWeek()
+        {
+            int[] lastWeek = new int[] { 0, 2, 5, 3, 7, 8, 4 };
+            return lastWeek;
+        }
+
+        public int Today()
+        {
+            return birdsPerDay[^1];
+        }
+
+        public void IncrementTodaysCount()
+        {
+            birdsPerDay[^1] += 1;
+        }
+
+        public bool HasDayWithoutBirds()
+        {
+            return birdsPerDay.Contains(0);
+        }
+
+        public int CountForFirstDays(int numberOfDays)
+        {
+            int counted = 0;
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                counted += birdsPerDay[i];
+            }
+            return counted;
+        }
+
+        public int BusyDays()
+        {
+            int buyDays = 0;
+            for (int i = 0; i < birdsPerDay.Length - 1; i++)
+            {
+                if (birdsPerDay[i] >= 5)
+                {
+                    buyDays += 1;
+                }
+            };
+            return buyDays;
+        }
+    }
+}
---
+++ OwnProjects/17_Arrays/ArraysApps/BirdCount/BirdCountCommunity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdCount
+{
+    internal class BirdCountCommunity
+    {
+        private int[] birdsPerDay;
+        public BirdCountCommunity(int[] birdsPerDay) => this.birdsPerDay = birdsPerDay;
+        public static int[] LastWeek() => new[] { 0, 2, 5, 3, 7, 8, 4 };
+        public int Today() => birdsPerDay.Last();
+        public void IncrementTodaysCount()
+        {
+            birdsPerDay[^1]++;
+        }
+        public bool HasDayWithoutBirds() => birdsPerDay.Contains(0);
+        public int CountForFirstDays(int numberOfDays) => birdsPerDay.Take(numberOfDays).Sum();
+        public int BusyDays() => birdsPerDay.Count(day => day >= 5);
+    }
+}
