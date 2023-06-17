fece36f5867c42b33d7be2f96fecf973c32cb0e9
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-17 10:21:09 -0500

    Proyect Resistor Two Colors

---
+++ OwnProjects/17_Arrays/ArraysApps/ResistorColorDuo/ResistorColorDuo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResistorColorDuo
+{
+    public static class ResistorColorDuo
+    {
+        private static readonly string[] ColorsArray =
+       {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        public static int Value(string[] colors)
+        {
+            string codeColor = "";
+            for (int i = 0; i <= 1; i++)
+            {
+                codeColor += Array.IndexOf(ColorsArray, colors[i].ToLower());
+            }
+            return int.Parse(codeColor);
+        }
+    }
+}
---
+++ OwnProjects/17_Arrays/ArraysApps/ResistorColorDuo/ResistorColorDuoCommunity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResistorColorDuo
+{
+    internal class ResistorColorDuoCommunity
+    {
+        public static int Value(string[] colors)
+        {
+            var value1 = GetColorValue(colors[0]) * 10;
+            var value2 = GetColorValue(colors[1]);
+            return value1 + value2;
+        }
+        private static int GetColorValue(string color) => (color.ToLower()) switch
+        {
+            "black" => 0,
+            "brown" => 1,
+            "red" => 2,
+            "orange" => 3,
+            "yellow" => 4,
+            "green" => 5,
+            "blue" => 6,
+            "violet" => 7,
+            "grey" => 8,
+            "white" => 9,
+            _ => -1,
+        };
+    }
+}
