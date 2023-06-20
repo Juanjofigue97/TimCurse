a9b001ddbff4bf24668bba6890f594f39436448a
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-20 17:37:29 -0500

    Booking Date

---
+++ OwnProjects/09_DataTime/DataTimeApps/BookingUpForBeauty/Appointment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingUpForBeauty
+{
+    public static class Appointment
+    {
+        public static DateTime Schedule(string appointmentDateDescription)
+        {
+            var Datetime = DateTime.Parse(appointmentDateDescription);
+            return Datetime;
+        }
+
+        public static bool HasPassed(DateTime appointmentDate) => appointmentDate < DateTime.Now;
+
+        public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour > 12 && appointmentDate.Hour<18;
+
+        public static string Description(DateTime appointmentDate)
+        {
+            return $"You have an appointment on {appointmentDate:G}";
+        }
+
+        public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
+    }
+}
---
+++ OwnProjects/09_DataTime/DataTimeApps/BookingUpForBeauty/AppointmentComunnity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingUpForBeauty
+{
+    internal class AppointmentComunnity
+    {
+        static class Appointment
+        {
+            public static DateTime Schedule(string appointmentDateDescription) => DateTime.Parse(appointmentDateDescription);
+            public static bool HasPassed(DateTime appointmentDate) => appointmentDate < DateTime.Now;
+            public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+            public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate}.";
+            public static DateTime AnniversaryDate() => new(DateTime.Today.Year, 09, 15);
+        }
+    }
+}
