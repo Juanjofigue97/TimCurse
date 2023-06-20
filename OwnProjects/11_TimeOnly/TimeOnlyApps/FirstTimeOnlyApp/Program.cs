705623c99fa99598f642666767a53a1f13d674ff
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-20 10:06:41 -0500

    TimeOnly Variable

--- OwnProjects/11_TimeOnly/TimeOnlyApps/FirstTimeOnlyApp/Program.cs
+++ OwnProjects/11_TimeOnly/TimeOnlyApps/FirstTimeOnlyApp/Program.cs
@@ -1,4 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-DateOnly dateOnly = new DateOnly();
+﻿
 
-Console.WriteLine(dateOnly.AddYears(2));
+
+var workTimeStart = new TimeOnly(8,0);
+
+Console.WriteLine($"The entry to work in the company is {workTimeStart}");
+
+var workTimeEnd = new TimeOnly(18, 0);
+
+Console.WriteLine($"The get out to work in the company is {workTimeEnd}");
+
+Console.WriteLine($"We work {workTimeEnd.Hour - workTimeStart.Hour} hours");
+
+// IsBetween: Is a handy method to quickly understand if a time falls in a range
+
+var sevenAM = new TimeOnly(7, 0);
+var elevenAM = new TimeOnly(11, 0);
+var onePM = new TimeOnly(13, 0);
+
+Console.WriteLine(elevenAM.IsBetween(sevenAM, onePM)); 
