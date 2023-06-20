7abba68cacb1c585a7ecd3cd5f8b5836aff850f8
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-20 10:30:20 -0500

    TryParse, Parse , Convert To

---
+++ OwnProjects/12_TypeConversions/ParseVariables/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+// int => double, decimal
+
+int age = 12;
+
+// double ageToDouble = (double) age;
+double ageToDouble = Convert.ToDouble(age);
+
+Console.WriteLine($"My age in double is {ageToDouble}");
+
+decimal ageToDecimal = (decimal) age;
+
+
+Console.WriteLine($"My age in decimal is {ageToDecimal}");
+
+
+string ageToString = "14";
+
+int ageconvert = int.Parse(ageToString);
+
+// Try Parse
+
+
+bool isAgeDecimal = decimal.TryParse("hola",out decimal ageDecimal);
+
+if (isAgeDecimal)
+{
+    Console.WriteLine($"Yes: {ageDecimal}");
+}
+
+
+var DateString = "2022-05-09";
+
+var Date = DateTime.Parse(DateString);
+
+Console.WriteLine(Date.AddYears(1));
+
+
+
+decimal area = 13.654M;
+
+int areaInt = (int) area;
+
+double areaToDouble = Convert.ToDouble(area);
+Console.WriteLine(areaInt);
+Console.WriteLine(areaToDouble);
