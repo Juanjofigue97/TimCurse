678ce9b8115919ab653d4baf8daf9c2d280f1856
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-20 10:06:04 -0500

    Using AddHours, AddMinutes, Comparisons, ticks

---
+++ OwnProjects/11_TimeOnly/TimeOnlyApps/ArithmeticOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+var start = new TimeOnly(8, 0);
+var end = new TimeOnly(12, 0);
+
+// Add hours
+
+Console.WriteLine($"The start time is {start}");
+Console.WriteLine($"The start time is {start.AddHours(1)} Add 1 Hour");
+Console.WriteLine($"The start time is {start.AddMinutes(65)} Add 61 minutes");
+
+if ( start < end)
+{
+    Console.WriteLine($"{start} is before {end}");
+}
+
+start = start.Add(new TimeSpan( 8, 3,2));
+
+if (start > end)
+{
+    Console.WriteLine($"{start} is after {end}");
+}
+
+// The number represent 10 It represents the total number of ticks
+Console.WriteLine(TimeOnly.MinValue.Ticks);
+Console.WriteLine(TimeOnly.MaxValue.Ticks);
+
