f86d1161e5171da174d8f3942549f05eb5702b2c
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-20 10:06:23 -0500

    Convert Data Time to TimeOnly

---
+++ OwnProjects/11_TimeOnly/TimeOnlyApps/ConvertDateTimeOnly/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+// Exercise: https://learn.microsoft.com/en-us/dotnet/standard/datetime/how-to-use-dateonly-timeonly
+// Convert DateTime to TimeOnly
+
+var now = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine($"It is {now} right now");
+
+var birthday = new DateTime(1997, 2, 4, 10, 5, 3);
+
+var birthhour = TimeOnly.FromDateTime(birthday);
+
+Console.WriteLine($"The time of birth is {birthhour}");
