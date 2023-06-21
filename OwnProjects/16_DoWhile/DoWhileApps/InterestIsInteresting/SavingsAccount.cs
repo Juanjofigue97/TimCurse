71c24a211a26b36eed06d7097e2f18329af01f34
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-21 17:50:31 -0500

    Do while

---
+++ OwnProjects/16_DoWhile/DoWhileApps/InterestIsInteresting/SavingsAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestIsInteresting
+{
+    public static class SavingsAccount
+    {
+        public static float InterestRate(decimal balance)
+        {
+            var interestRate = balance switch
+            {
+                < 0 => 3.213f,
+                < 1000 => 0.5f,
+                < 5000 => 1.621f,
+                >= 5000 => 2.475f,
+            };
+
+            return interestRate;
+        }
+
+        public static decimal Interest(decimal balance)
+        {
+            return balance * (decimal)InterestRate(balance) / 100;
+        }
+
+        public static decimal AnnualBalanceUpdate(decimal balance)
+        {
+            return Interest(balance) + balance;
+        }
+
+        public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+        {
+            int years = 1;
+            decimal TotalBalance = AnnualBalanceUpdate(balance);
+            do
+            {
+                TotalBalance += Interest(TotalBalance);
+                years++;
+
+            } while (TotalBalance < targetBalance);
+            return years;
+        }
+    }
+}
