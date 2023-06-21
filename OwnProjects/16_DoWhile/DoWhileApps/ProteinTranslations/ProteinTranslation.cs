be185a1b93f007fb92620e87c989cafdf53deebb
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-21 17:51:05 -0500

    Do While Exercies

---
+++ OwnProjects/16_DoWhile/DoWhileApps/ProteinTranslations/ProteinTranslation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProteinTranslations
+{
+    public static class ProteinTranslation
+    {
+        public static string[] Proteins(string strand)
+        {
+            var proteinTranslations = new Dictionary<string, string>
+            {
+                ["Methionine"] = "AUG",
+                ["Phenylalanine"] = "UUU,UUC",
+                ["Leucine"] = "UUA,UUG",
+                ["Serine"] = "UCU, UCC, UCA, UCG",
+                ["Tyrosine"] = "UAU, UAC",
+                ["Cysteine"] = "UGU, UGC",
+                ["Tryptophan"] = "UGG",
+                ["STOP"] = "UAA, UAG, UGA"
+            };
+
+            //List<string> list;
+
+            //list.Add();
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
