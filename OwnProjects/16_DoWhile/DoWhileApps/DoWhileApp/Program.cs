13cc56cb104c563c77702f96d6ab8136be6078b3
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-21 17:50:06 -0500

    Do while Apps

---
+++ OwnProjects/16_DoWhile/DoWhileApps/DoWhileApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+bool isValidNumber = false;
+int number = default;
+do
+{
+    Console.Write("Write a number: ");
+    isValidNumber = int.TryParse(Console.ReadLine(),out number);
+
+} while (!isValidNumber);
+
+int range = 1;
+do
+{
+    Console.WriteLine($"Number is : {range} and cube of the 1 is {range*range*range}");
+    range++;
+}while (range <= number);
