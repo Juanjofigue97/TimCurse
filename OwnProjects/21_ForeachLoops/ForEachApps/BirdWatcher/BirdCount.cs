42ac18dcc9b5967f9a9b8f24e80b539c11eaeb09
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:35:25 -0500

    Testing the use of foreach with different collections

---
+++ OwnProjects/21_ForeachLoops/ForEachApps/BirdWatcher/BirdCount.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+namespace BirdWatcher
+{
+    public class BirdCount
+    {
+        private int[] birdsPerDay;
+
+        public BirdCount(int[] birdsPerDay)
+        {
+            this.birdsPerDay = birdsPerDay;
+        }
+
+        public static int[] LastWeek()
+        {
+            int[] lastWeek = new int[] { 0, 2, 5, 3, 7, 8, 4 };
+            return lastWeek;
+        }
+
+        public int Today() => birdsPerDay[^1];
+
+        public void IncrementTodaysCount() => birdsPerDay[^1]++;
+
+        public bool HasDayWithoutBirds() => birdsPerDay.Contains(0);
+
+        public int CountForFirstDays(int numberOfDays)
+        {
+            var list = birdsPerDay[..numberOfDays];
+            return list.Sum();
+        }
+
+        public int BusyDays()
+        {
+            var buyDays = birdsPerDay.Where(day => day >= 5);
+            return buyDays.Count();
+        }
+    }
+}
---
+++ OwnProjects/21_ForeachLoops/ForEachApps/ForEachApp/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+int[] vals = { 1, 2, 3, 4, 5 };
+
+foreach (var val in vals)
+{
+    Console.WriteLine(val);
+}
+
+var words = new List<string> { "tea", "falcon", "book", "sky" };
+
+words.ForEach(x => Console.WriteLine(x));
+
+foreach (var word in words)
+{
+    Console.WriteLine(word);
+}
+
+var wordsLists = new List<List<string>>
+{
+    new List<string>{"tea", "falcon", "book", "sky"},
+    new List<string>{"cup", "crown", "borrow", "moore"},
+    new List<string>{"arm", "nice", "frost", "sea"}
+};
+
+foreach (var nested in wordsLists)
+{
+    Console.WriteLine(nested.FirstOrDefault());
+    foreach (var word in nested)
+    {
+        Console.WriteLine(word);
+    }
+}
+
+
+var domains = new Dictionary<string, string>
+{
+    {"sk", "Slovakia"},
+    {"ru", "Russia"},
+    {"de", "Germany"},
+    {"no", "Norway"}
+};
+
+foreach (var pair in domains)
+{
+    Console.WriteLine($"{pair.Key} - {pair.Value}");
+}
+
+Console.WriteLine("-----------------------");
+
+foreach ((var Key, var Value) in domains)
+{
+    Console.WriteLine($"{Key} - {Value}");
+}
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+Array.ForEach(numbers, e => Console.WriteLine(e));
+
+domains.ToList().ForEach(e => Console.WriteLine(e.Value));
