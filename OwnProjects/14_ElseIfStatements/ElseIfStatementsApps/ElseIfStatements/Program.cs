39399ec2948d2677e71d105d82ddc5bc5e88fcda
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:40:57 -0500

    Learning to use Else If Statements

---
+++ OwnProjects/14_ElseIfStatements/ElseIfStatementsApps/ElseIfStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+Console.WriteLine("In this program, I will test 'if' and 'else if' statements.");
+Console.WriteLine("This program was built by Juan Jose Figueroa");
+Console.WriteLine("------------------------------------------------");
+Console.WriteLine();
+
+bool isValid = false;
+int age = 0;
+while (!isValid)
+{
+    Console.Write("Check if the person is of legal age: ");
+    isValid = int.TryParse(Console.ReadLine(), out age);
+}
+
+if (age == 0)
+{
+    Console.WriteLine("You are not even one year old!");
+}
+else if (age > 0 && age < 10)
+{
+    Console.WriteLine("You are less than a decade old!");
+}
+else if (age >= 10 && age < 17)
+{
+    Console.WriteLine($"You need {18 - age} more years to reach the legal age!");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("You are of legal age!");
+}
+else
+{
+    Console.WriteLine("You did not enter a valid age.");
+}
+
+Console.WriteLine("------------------------------------------------");
+Console.WriteLine("Thank you for using my program. See you next time!");
+Console.ReadLine();
---
+++ OwnProjects/14_ElseIfStatements/ElseIfStatementsApps/TemperatureCentigrade/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+Console.WriteLine("In this program, The user give a temperature in centigrade and display a suitable message according to the temperature state below.");
+Console.WriteLine("This program was built by Juan Jose Figueroa");
+Console.WriteLine("------------------------------------------------");
+Console.WriteLine();
+
+Console.Write("Please enter the temperature in Celsius degrees: ");
+
+int Celsius = int.Parse(Console.ReadLine());
+
+if (Celsius < 0)
+{
+    Console.WriteLine("Freezing weather");
+}
+else if (Celsius >= 0 && Celsius < 10)
+{
+    Console.WriteLine("Very Cold weather");
+}
+else if (Celsius >= 10 && Celsius < 20)
+{
+    Console.WriteLine("Cold weather");
+}
+else if (Celsius >= 20 && Celsius < 30)
+{
+    Console.WriteLine("Normal in Temp");
+}else if (Celsius >= 30 && Celsius < 40)
+{
+    Console.WriteLine("Its Hot");
+}else if(Celsius >= 40)
+{
+    Console.WriteLine("Its Very Hot");
+}
+else
+{
+    Console.WriteLine("You did not enter a valid Temperature.");
+}
