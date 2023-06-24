b13d2b25c72a3268f0d171ed4dc51aa0781471af
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:39:43 -0500

    Learning Switch Loops

---
+++ OwnProjects/15_SwitchStatements/SwitchApps/BankAccount/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+using BankAccount;
+
+Console.WriteLine("Bank Account");
+
+decimal interesRate = 12.32M;
+Console.WriteLine(SavingsAccount.InterestRate(interesRate));
---
+++ OwnProjects/15_SwitchStatements/SwitchApps/SwitchApp/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+Console.WriteLine("Create a C# ");
+
+int number = 9;
+
+switch (number)
+{
+    case 0 or 1 or 2 or 3 or 4:
+        Console.WriteLine($"Fail");
+        break;
+    case 5:
+        Console.WriteLine($"Pass");
+        break;
+    case 6:
+        Console.WriteLine($"Good");
+        break;
+    case 7 or 8:
+        Console.WriteLine($"Excellent");
+        break;
+    case 9 or 10:
+        Console.WriteLine($"Outstanding");
+        break;
+}
+
+// Simply Swtich case
+
+var Menssage = number switch
+{
+    9 or 10 => "Outstanding",
+    7 or 8 => "Excellent",
+    6 => "Good",
+    5 => "Pass",
+    _ => "Fail"
+};
+Console.WriteLine(Menssage);
