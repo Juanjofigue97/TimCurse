eae7ccc005f84af1001471ff4a79f403b406d17f
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:37:01 -0500

    Writing the names of all Users to the console

---
+++ Homeworks/ForEach_Loops/ForEachLoopsApps/FirstNameHelloAllNames/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+Console.WriteLine("This program will greet all the Users.");
+Console.WriteLine("This program was built by Juan Jose Figueroa");
+Console.WriteLine("---------------------------------------------------");
+Console.WriteLine();
+
+
+List<string> names = new();
+bool isFinish = false;
+while (!isFinish)
+{
+    Console.Write("Please enter the name of user: ");
+    string name = Console.ReadLine();
+    names.Add(name);
+
+    Console.Write("Would you like to continue entering names? y/n: ");
+    string valid = Console.ReadLine();
+
+    isFinish = valid.ToUpper() == "Y" ? false:true;
+}
+Console.WriteLine();
+Console.WriteLine("---------------------------------------------");
+foreach (var item in names)
+{
+    Console.WriteLine($"Hello {item} Thank you for coming");
+}
+
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Thank you for using my program. See you next time!");
