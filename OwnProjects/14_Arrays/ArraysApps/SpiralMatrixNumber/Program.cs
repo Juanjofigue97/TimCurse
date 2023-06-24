d7fd9fe7acfc74196d74a7b81be8f3ee66010172
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:40:07 -0500

    Numbers Spiral

---
+++ OwnProjects/14_Arrays/ArraysApps/SpiralMatrixNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using SpiralMatrixNumber;
+
+var matrix = SpritalMatrix.GetMatrix(2);
+
+
+for (int fila = 0; fila < matrix.GetLength(0); fila++)
+{
+    for (int columna = 0; columna < matrix.GetLength(1); columna++)
+    {
+        Console.Write($"{matrix[fila, columna]} ");
+    }
+    Console.WriteLine();
+}
---
+++ OwnProjects/14_Arrays/ArraysApps/SpiralMatrixNumber/SpritalMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralMatrixNumber
+{
+    internal class SpritalMatrix
+    {
+        public static int[,] GetMatrix(int size)
+        {
+            if (size == 0)
+            {
+                return new int[size,size];
+            };
+
+            var matrix = new int[size, size];
+            int column = 0;
+            int row = 0;
+            for (int i = 1; i <= size*2; i++)
+            {
+                if (row == size)
+                {
+                    return matrix;
+                }
+                matrix[row, column] = i;
+                column++;
+                if (column == size)
+                {
+                    column = 0;
+                    row += 1;
+                }
+                
+            }
+
+            return matrix;
+        }
+    }
+}
