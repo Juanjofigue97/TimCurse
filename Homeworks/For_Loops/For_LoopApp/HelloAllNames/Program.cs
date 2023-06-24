25fba143c538fe8aadb728d0fa1c40c620fb7f4e
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-24 10:37:28 -0500

    Writing the names of all Users to the console but using for Loop

---
+++ Homeworks/For_Loops/For_LoopApp/HelloAllNames/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("This program will greet all the guests at the party.");
+Console.WriteLine("This program was built by Juan Jose Figueroa");
+Console.WriteLine("---------------------------------------------------");
+Console.WriteLine();
+Console.Write("Please enter the names of all the people at the party: ");
+string names = Console.ReadLine();
+
+
+var ArrayNames = names.Split(',');
+
+
+for (int i = 0; i < ArrayNames.Length; i++)
+{
+    Console.WriteLine($"Hello {ArrayNames[i]}! Thank you for coming.");
+}
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Thank you for using my program. See you next time!");
