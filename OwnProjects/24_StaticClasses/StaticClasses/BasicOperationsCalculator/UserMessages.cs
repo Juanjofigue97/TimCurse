9e7aac64ba2a4a304996e251ec90f0653e1e090f
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-26 17:54:51 -0500

    Static class with the only purpose show messages

---
+++ OwnProjects/24_StaticClasses/StaticClasses/BasicOperationsCalculator/UserMessages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace BasicOperationsCalculator
+{
+    public static class UserMessages
+    {
+        public static void ApplicationStartMessage(string firstName)
+        {
+            Console.Clear();
+            Console.WriteLine($"Welcome to the Static Class Demo App");
+
+            int hourDay = DateTime.Now.Hour;
+
+            var output = hourDay switch
+            {
+                < 12 => $"Good morning { firstName }!",
+                < 19 => $"Good afternoon {firstName}!",
+                _ => $"Good evening  {firstName} !"
+            };
+
+            Console.WriteLine(output);
+        }
+
+        public static void PrintResultMessage(string message) 
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("--------------------------------");
+        }
+        public static void ApplicacionFinishMessage()
+        {
+            Console.WriteLine("Thank four using our app to calculate for you");
+
+        }
+    }
+}
