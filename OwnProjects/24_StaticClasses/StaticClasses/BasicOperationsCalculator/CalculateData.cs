ac12f6f790fa83f054d300b8154a467d2a8a4bda
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-26 17:55:11 -0500

    Work with the Basic Operation

---
+++ OwnProjects/24_StaticClasses/StaticClasses/BasicOperationsCalculator/CalculateData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperationsCalculator
+{
+    public static class CalculateData
+    {
+        public static double Add(double x, double y)
+        {
+            double output = x + y;
+            return output;
+        }
+        public static double Subtract(double x, double y)
+        {
+            double output = x - y;
+            return output;
+        }
+        public static double Multiply(double x, double y)
+        {
+            double output = x * y;
+            return output;
+        }
+        public static double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+                return double.NaN;
+            }
+
+            double output = x / y;
+            return output;
+        }
+    }
+}
