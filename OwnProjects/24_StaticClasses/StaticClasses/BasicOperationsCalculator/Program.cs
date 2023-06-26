1c780cea5c487d243caca3cdc0b665d04c289763
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-26 17:54:32 -0500

    Main Class

---
+++ OwnProjects/24_StaticClasses/StaticClasses/BasicOperationsCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using BasicOperationsCalculator;
+
+string firstName = RequestData.GetAStrig("What is your first name: ");
+
+UserMessages.ApplicationStartMessage(firstName);
+
+double x = RequestData.GetADouble("Please enter your first number: ");
+double y = RequestData.GetADouble("Please enter your second number: ");
+
+double resultAdd = CalculateData.Add(x, y);
+UserMessages.PrintResultMessage($"The sum of { x } and { y } is { resultAdd }");
+
+double resultSubtract = CalculateData.Subtract(x, y);
+UserMessages.PrintResultMessage($"The subtract of { x } and { y } is { resultSubtract }");
+
+double resultMultiply = CalculateData.Multiply(x, y);
+UserMessages.PrintResultMessage($"The multiply of { x } and { y } is { resultMultiply }");
+
+double resultDivide = CalculateData.Divide(x, y);
+UserMessages.PrintResultMessage($"The divide of { x } and { y } is { resultDivide }");
+
+
+Console.ReadLine();
