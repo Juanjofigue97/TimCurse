d4d69b83b34b955ce2113ea55da831a1a55d64d6
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-27 08:58:51 -0500

    Nested Tuples

---
+++ OwnProjects/23_Tuples/TuplesApps/NestedTuples/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+var data = ((1, 2, 3), (3, 4, 5), (6, 7, 8));
+Console.WriteLine(data);
+
+Console.WriteLine(data.Item1);
+Console.WriteLine(data.Item2);
+Console.WriteLine(data.Item1);
+
+Console.WriteLine(data.Item1.Item1);
+Console.WriteLine(data.Item2.Item3);
