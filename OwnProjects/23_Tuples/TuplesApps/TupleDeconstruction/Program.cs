b191237c2f1c7c50298dde58ae2ca8cd88bbc5b9
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-27 08:59:07 -0500

    Deconstruction Tuple

---
+++ OwnProjects/23_Tuples/TuplesApps/TupleDeconstruction/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+var (name1, age1) = ("John Doe", 34);
+Console.WriteLine($"{name1} is {age1} years old");
+
+(string name2, int age2) = ("Roger Roe", 55);
+Console.WriteLine($"{name2} is {age2} years old");
+
+string name3;
+int age3;
+
+(name3, age3) = ("Jane Doe", 26);
+Console.WriteLine($"{name3} is {age3} years old");
