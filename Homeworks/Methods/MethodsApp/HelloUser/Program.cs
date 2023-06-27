df033968a99504853b6cb23f3c8a2b261805ed96
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-27 08:57:47 -0500

    Using methods gives us a cleaner solution

---
+++ Homeworks/Methods/MethodsApp/HelloUser/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using HelloUser;
+
+UserInteraction.AppStartMessage();
+
+string userName = UserInteraction.AskUserName();
+
+UserInteraction.HelloUser(userName);
+
+UserInteraction.SayGoodBye();
+
+Console.ReadLine();
---
+++ Homeworks/Methods/MethodsApp/HelloUser/UserInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUser
+{
+    public static class UserInteraction
+    {
+        public static void AppStartMessage() 
+        {
+            Console.WriteLine("Hello, in this program, I'm using methods. \nThis application was created by Juan Jose Figueroa.");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+        }
+        public static string AskUserName()
+        {
+            Console.Write("What is your first name: ");
+            string nameUser = Console.ReadLine();
+            return nameUser;
+        }
+        public static void HelloUser(string nameUser) 
+        {
+            Console.WriteLine($"Hello {nameUser} !");
+        }  
+
+        public static void SayGoodBye()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Thank you for using this program. See you next time!");
+            Console.WriteLine();
+        }
+    }
+}
