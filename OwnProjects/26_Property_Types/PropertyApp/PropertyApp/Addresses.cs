a567aea758916902d61c5639a4cf3207cedb877f
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-28 19:47:33 -0500

    OPP Property Types

---
+++ OwnProjects/26_Property_Types/PropertyApp/PropertyApp/Addresses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp
+{
+    public class Addresses
+    {
+        public Addresses(string city, string address)
+        {
+            City = city;
+            Address = address;
+        }
+
+        public string Street { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        private int _numberHouse;
+
+        public int NumberHouse
+        {
+            get { return _numberHouse; }
+            set { _numberHouse = value; }
+        }
+
+
+        public string FullAddress
+        {
+            get { 
+                return $"{Address} - {City} || Postal Code: {PostalCode}"; 
+            }
+        }
+
+    }
+}
---
+++ OwnProjects/26_Property_Types/PropertyApp/PropertyApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+using PropertyApp;
+
+var homeAddress = new Addresses("Pasto", "Calle 9 No.42-20");
+
+homeAddress.Street = "Calle 11";
+homeAddress.PostalCode = "12345";
+Console.WriteLine($"Full Address: {homeAddress.FullAddress}");
