4ddc7d468153cd10fb87d04cfe1ceddf59a06e75
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-06-28 21:55:52 -0500

    Using a Library to Add Models

---
+++ OwnProjects/27_ClassLibraries/ClassLibraryDemo/ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using DemoLibrary.Models.UserModel;
+
+Console.WriteLine("We are work with ClassLibrary");
+
+var person = new User()
+{
+    FirstName = "Juan",
+    LastName = "Figueroa"
+};
+
+Console.WriteLine($"Hello {person.FirstName} {person.LastName}! ");
+
+var address = new Addresses();
---
+++ OwnProjects/27_ClassLibraries/ClassLibraryDemo/DemoLibrary/Models/UserModel/Acitity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Models.UserModel
+{
+    public class Acitity
+    {
+        public int ActivityID { get; set; }
+        public TimeSpan ActivityTimeSpan { get; set; }
+
+    }
+}
