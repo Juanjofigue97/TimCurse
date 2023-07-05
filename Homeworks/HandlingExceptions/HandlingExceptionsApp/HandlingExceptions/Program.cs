24f635a47fd1e7a806bd04c990865b98878ee63e
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-05 16:55:55 -0500

    Caching Exptions

---
+++ Homeworks/HandlingExceptions/HandlingExceptionsApp/HandlingExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+Console.WriteLine("We are Catch Exceptions");
+
+List<int> listNumbers = new()
+{
+    1, 2, 3, 4, 5
+};
+
+try
+{
+    for (int i = 0; i < 6; i++)
+    {
+        Console.WriteLine($"The number is {listNumbers[i]}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"The expection was {ex.Message}");
+}
+
