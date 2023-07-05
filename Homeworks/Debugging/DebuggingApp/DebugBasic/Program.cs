4a553fc09ba310e92cd5af75a2d38d6b6382252f
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-05 16:47:34 -0500

    Learning How Debug

--- Homeworks/Debugging/DebuggingApp/DebugBasic/Program.cs
+++ Homeworks/Debugging/DebuggingApp/DebugBasic/Program.cs
@@ -1,3 +1,16 @@
 ﻿
 
-Console.WriteLine();
+Console.WriteLine("This program I'm going to Practice Debbug");
+
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; i++)
+    {
+        Console.WriteLine(j);
+    }
+}
+
+
+Console.WriteLine("The program end.");
+Console.ReadLine();
---
+++ Homeworks/Debugging/DebuggingApp/MutipliesFive/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("This program I'm going to Practice Debbug");
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int totalSum = 0;
+
+
+for (int i = 0; i < 10; i++)
+{
+    totalSum += 5 * number;
+}
+
+Console.WriteLine($"The total sum is { totalSum }");
+
+Console.WriteLine("The program end.");
+Console.ReadLine();
