a11ba106a83536de72b176ac4533ed5713ccaaed
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-06 20:44:28 -0500

    Practicing exception handling.

---
+++ Homeworks/HandlingExceptions/AdvanceExeptions/AdvanceExeptionsApp/AdvanceExeptions/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace AdvanceExeptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In this program, we will capture exceptions");
+            Console.WriteLine("This program was created by Juan Jose Figueroa. ");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Call Method");
+
+            try
+            {
+                Console.Write("How old are you? ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                GetAge(age);
+                GetFile();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Any Exception: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("The program End... Thanks for comming ");
+            }
+
+
+            Console.ReadLine();
+
+        }
+        public static void GetFile()
+        {
+            throw new FileNotFoundException("The File was not Found");
+        }
+        public static void GetAge(int age)
+        {
+            if (age < 0)
+            {
+              throw new IndexOutOfRangeException("You are out of the appropriate range");
+            }
+            else
+            {
+                Console.WriteLine($"Yor age is { age }");
+            }
+        }
+    }
+}
