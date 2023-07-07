08aa22fb0e1c59964ebc655104f89585dc6eca09
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-07 18:04:08 -0500

    Using Arrays

---
+++ OwnProjects/14_Arrays/ArraysApps/AnagramApp/Anagram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramApp
+{
+    public class Anagram
+    {
+        public string Word { get; set; }
+        public Anagram(string baseWord)
+        {
+            Word = baseWord;
+        }
+
+        public string[] FindAnagrams(string[] potentialMatches)
+        {
+            List<char> chars = Word.ToLower().ToList();
+            List<string> goodMatches = new ();
+
+            foreach (var word in potentialMatches)
+            {
+                if (word.ToLower() == Word.ToLower())
+                {
+                    continue;
+                }
+                List<char> letters = word.ToLower().ToList();
+                bool isAnagram = false;
+                chars = Word.ToLower().ToList();
+                foreach (var letter in letters) 
+                {
+                    if (chars.Contains(letter))
+                    {
+                        var index = chars.IndexOf(letter);
+                        chars.RemoveAt(index);
+
+                        if (chars.Count == 0)
+                        {
+                            isAnagram = true;
+                        }
+                    }
+                    else
+                    {
+                        isAnagram = false;
+                    }
+                }
+                if (isAnagram)
+                {
+                    goodMatches.Add(word);
+                }
+
+            }
+            return goodMatches.ToArray();
+        }
+    }
+}
---
+++ OwnProjects/14_Arrays/ArraysApps/AnagramApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using AnagramApp;
+
+var candidates = new[]
+        {
+            "dog",
+            "goody"
+        };
+var sut = new Anagram("good");
+
+
+foreach (var word in sut.FindAnagrams(candidates))
+{
+    Console.WriteLine(word);
+}
