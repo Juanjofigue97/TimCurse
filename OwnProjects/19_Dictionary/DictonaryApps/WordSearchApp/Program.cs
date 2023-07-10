56b1db06e0bac6750304dfea7dda4a326744a388
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-10 18:07:47 -0500

    WordSeachApp

---
+++ OwnProjects/19_Dictionary/DictonaryApps/WordSearchApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using WordSearchApp;
+
+var wordsToSearchFor = new[] { "clojure" };
+var grid = "jefblpepre\n" +
+            "tclojurerm";
+var sut = new WordSearch(grid);
+
+foreach (var word in sut.Grid)
+{
+    Console.WriteLine(word);
+}
+
+//var actual = sut.Search(wordsToSearchFor);
+//var expected = new Dictionary<string, ((int, int), (int, int))?>
+//{
+//    ["clojure"] = ((1, 1), (7, 1))
+//};
---
+++ OwnProjects/19_Dictionary/DictonaryApps/WordSearchApp/WordSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearchApp
+{
+    public class WordSearch
+    {
+        public Dictionary<(int, int)? ,char> Grid { get; set; } = new Dictionary<(int, int)?, char>();
+        public WordSearch(string grid)
+        {
+            
+            int x = 1;
+            int y = 1;
+            foreach (var word in grid)
+            {
+                Console.WriteLine(word);
+                Grid.Add ((x, y),word);
+                y++;
+            }
+        }
+
+        public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
