8b5bb946b5d94c4d25a3a4e83eadd8319c369dcf
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-10 17:25:42 -0500

    Learning from the community

---
+++ OwnProjects/14_Arrays/ArraysApps/AnagramApp/AnagramCommunity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramApp
+{
+    public class AnagramCommunity
+    {
+        private readonly string baseWord;
+        public AnagramCommunity(string baseWord)
+        {
+            this.baseWord = baseWord.ToLower();
+        }
+        public string[] FindAnagrams(string[] potentialMatches) =>
+            potentialMatches
+                .Where(s => isAnagram(s.ToLower()))
+                .ToArray();
+        private bool isAnagram(string candidate) =>
+            candidate == baseWord ? false : (
+                string.Concat(candidate.OrderBy(c => c)) == string.Concat(baseWord.OrderBy(c => c))
+            );
+    }
+}
