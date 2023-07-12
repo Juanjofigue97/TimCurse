4d2000caf4a7b27539194aa15cb52eadc7c45bfb
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-12 14:19:24 -0500

    Override the "ToString()" method

---
+++ Homeworks/Inheritance_InterfaceMiniProject/MethodOverridingApp/MethodOverriding/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"FullName: { Name } { LastName } | Age: { Age }";
+        }
+    }
+}
---
+++ Homeworks/Inheritance_InterfaceMiniProject/MethodOverridingApp/MethodOverriding/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using MethodOverriding;
+
+Console.WriteLine("In this program, we are going to override the ToString() method that exists in all C# objects.");
+Console.WriteLine("This program was made by Juan Jose Figueroa.");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine();
+
+PersonModel person = new()
+{
+    Name = "Juan",
+    LastName = "Figueroa",
+    Age = 26,
+};
+
+Console.WriteLine("We are going to override the ToString() method. ");
+
+Console.WriteLine(person.ToString());
+
+
+
+
+Console.ReadLine();
