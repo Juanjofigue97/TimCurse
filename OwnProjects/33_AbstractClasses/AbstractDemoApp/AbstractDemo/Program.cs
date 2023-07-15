370ff6c5383a7040ebadf48f25b4bd7d2f4e58f5
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-15 10:51:48 -0500

    AbstractDemoApp

---
+++ OwnProjects/33_AbstractClasses/AbstractDemoApp/AbstractDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using AbstractDemo;
+
+Dog tobby = new Dog();
+
+tobby.MakeSound();
+
+Cat cat = new Cat();
+
+cat.MakeSound();
+
+//You cannot instantiate an abstract class.
+// Animal cat = new Animal();
+
+
+Console.ReadLine();
