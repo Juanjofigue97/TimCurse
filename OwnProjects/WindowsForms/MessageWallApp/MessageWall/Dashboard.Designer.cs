1fe4b59c54bf79bcdc69a0465f05de17b71dabe8
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-16 23:45:44 -0500

    My First WindosForms

---
+++ OwnProjects/WindowsForms/MessageWallApp/MessageWall/Dashboard.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MessageWall
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nessageLabel = new System.Windows.Forms.Label();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.messageListBox = new System.Windows.Forms.ListBox();
+            this.messageListBoxLabel = new System.Windows.Forms.Label();
+            this.addMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nessageLabel
+            // 
+            this.nessageLabel.AutoSize = true;
+            this.nessageLabel.Location = new System.Drawing.Point(36, 39);
+            this.nessageLabel.Name = "nessageLabel";
+            this.nessageLabel.Size = new System.Drawing.Size(112, 29);
+            this.nessageLabel.TabIndex = 0;
+            this.nessageLabel.Text = "Message";
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(154, 36);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(213, 35);
+            this.messageText.TabIndex = 1;
+            // 
+            // messageListBox
+            // 
+            this.messageListBox.FormattingEnabled = true;
+            this.messageListBox.ItemHeight = 29;
+            this.messageListBox.Location = new System.Drawing.Point(41, 153);
+            this.messageListBox.Name = "messageListBox";
+            this.messageListBox.Size = new System.Drawing.Size(326, 207);
+            this.messageListBox.TabIndex = 4;
+            this.messageListBox.TabStop = false;
+            // 
+            // messageListBoxLabel
+            // 
+            this.messageListBoxLabel.AutoSize = true;
+            this.messageListBoxLabel.Location = new System.Drawing.Point(36, 121);
+            this.messageListBoxLabel.Name = "messageListBoxLabel";
+            this.messageListBoxLabel.Size = new System.Drawing.Size(112, 29);
+            this.messageListBoxLabel.TabIndex = 3;
+            this.messageListBoxLabel.Text = "Message";
+            // 
+            // addMessage
+            // 
+            this.addMessage.Location = new System.Drawing.Point(373, 34);
+            this.addMessage.Name = "addMessage";
+            this.addMessage.Size = new System.Drawing.Size(71, 38);
+            this.addMessage.TabIndex = 2;
+            this.addMessage.Text = "Add";
+            this.addMessage.UseVisualStyleBackColor = true;
+            this.addMessage.Click += new System.EventHandler(this.AddMessage_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(505, 418);
+            this.Controls.Add(this.addMessage);
+            this.Controls.Add(this.messageListBoxLabel);
+            this.Controls.Add(this.messageListBox);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.nessageLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Dashboard";
+            this.Text = "Hello";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nessageLabel;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.ListBox messageListBox;
+        private System.Windows.Forms.Label messageListBoxLabel;
+        private System.Windows.Forms.Button addMessage;
+    }
+}
+
---
+++ OwnProjects/WindowsForms/MessageWallApp/MessageWall/Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageWall
+{
+    public partial class Dashboard : Form
+    {
+        BindingList<string> messages = new BindingList<string>();
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            WireUpLists();
+        }
+        private void WireUpLists()
+        {
+            //messages.Add("First message");
+            messageListBox.DataSource = messages;
+            messageListBox.DisplayMember = nameof(Dashboard.Text);
+        }
+
+        private void AddMessage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(messageText.Text))
+            {
+                MessageBox.Show("Please enter a message before trying to add it to the List.","Blank Message Field",MessageBoxButtons.OK,MessageBoxIcon.Error);
+               
+            }else
+            {
+                messages.Add(messageText.Text);
+                messageText.Text = "";
+            }
+            messageText.Focus();
+        }
+    }
+}
