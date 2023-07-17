9c70c5acf3d779e86ab269436d9f55b4a0c45314
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-17 18:02:50 -0500

    Windows Forms Desktop Information

---
+++ OwnProjects/WindowsForms/CofinalDesktopApp/InformacionEquipo/Dashboard.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace InformacionEquipo
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.ObtenerButton = new System.Windows.Forms.Button();
+            this.NombreUsuarioLabel = new System.Windows.Forms.Label();
+            this.IpLabel = new System.Windows.Forms.Label();
+            this.DominioLabel = new System.Windows.Forms.Label();
+            this.NombreComputadorLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NombreUsuarioText = new System.Windows.Forms.TextBox();
+            this.IpText = new System.Windows.Forms.TextBox();
+            this.NombreDominioText = new System.Windows.Forms.TextBox();
+            this.NombreComputadorText = new System.Windows.Forms.TextBox();
+            this.CarpetasCompartidasListaBox = new System.Windows.Forms.ListBox();
+            this.Infolabel = new System.Windows.Forms.Label();
+            this.LogoCofinal = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoCofinal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ObtenerButton
+            // 
+            this.ObtenerButton.Location = new System.Drawing.Point(72, 165);
+            this.ObtenerButton.Name = "ObtenerButton";
+            this.ObtenerButton.Size = new System.Drawing.Size(118, 40);
+            this.ObtenerButton.TabIndex = 1;
+            this.ObtenerButton.Text = "Obtener";
+            this.ObtenerButton.UseVisualStyleBackColor = true;
+            this.ObtenerButton.Click += new System.EventHandler(this.ObtenerButton_Click);
+            // 
+            // NombreUsuarioLabel
+            // 
+            this.NombreUsuarioLabel.AutoSize = true;
+            this.NombreUsuarioLabel.Location = new System.Drawing.Point(67, 234);
+            this.NombreUsuarioLabel.Name = "NombreUsuarioLabel";
+            this.NombreUsuarioLabel.Size = new System.Drawing.Size(190, 29);
+            this.NombreUsuarioLabel.TabIndex = 2;
+            this.NombreUsuarioLabel.Text = "Nombre Usuario";
+            // 
+            // IpLabel
+            // 
+            this.IpLabel.AutoSize = true;
+            this.IpLabel.Location = new System.Drawing.Point(67, 278);
+            this.IpLabel.Name = "IpLabel";
+            this.IpLabel.Size = new System.Drawing.Size(35, 29);
+            this.IpLabel.TabIndex = 3;
+            this.IpLabel.Text = "IP";
+            // 
+            // DominioLabel
+            // 
+            this.DominioLabel.AutoSize = true;
+            this.DominioLabel.Location = new System.Drawing.Point(67, 318);
+            this.DominioLabel.Name = "DominioLabel";
+            this.DominioLabel.Size = new System.Drawing.Size(103, 29);
+            this.DominioLabel.TabIndex = 4;
+            this.DominioLabel.Text = "Dominio";
+            // 
+            // NombreComputadorLabel
+            // 
+            this.NombreComputadorLabel.AutoSize = true;
+            this.NombreComputadorLabel.Location = new System.Drawing.Point(67, 357);
+            this.NombreComputadorLabel.Name = "NombreComputadorLabel";
+            this.NombreComputadorLabel.Size = new System.Drawing.Size(240, 29);
+            this.NombreComputadorLabel.TabIndex = 5;
+            this.NombreComputadorLabel.Text = "Nombre Computador";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 397);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Carpetas Compartidas";
+            // 
+            // NombreUsuarioText
+            // 
+            this.NombreUsuarioText.Location = new System.Drawing.Point(353, 231);
+            this.NombreUsuarioText.Name = "NombreUsuarioText";
+            this.NombreUsuarioText.Size = new System.Drawing.Size(304, 35);
+            this.NombreUsuarioText.TabIndex = 7;
+            // 
+            // IpText
+            // 
+            this.IpText.Location = new System.Drawing.Point(353, 278);
+            this.IpText.Name = "IpText";
+            this.IpText.Size = new System.Drawing.Size(304, 35);
+            this.IpText.TabIndex = 8;
+            // 
+            // NombreDominioText
+            // 
+            this.NombreDominioText.Location = new System.Drawing.Point(353, 318);
+            this.NombreDominioText.Name = "NombreDominioText";
+            this.NombreDominioText.Size = new System.Drawing.Size(304, 35);
+            this.NombreDominioText.TabIndex = 9;
+            // 
+            // NombreComputadorText
+            // 
+            this.NombreComputadorText.Location = new System.Drawing.Point(353, 357);
+            this.NombreComputadorText.Name = "NombreComputadorText";
+            this.NombreComputadorText.Size = new System.Drawing.Size(304, 35);
+            this.NombreComputadorText.TabIndex = 10;
+            // 
+            // CarpetasCompartidasListaBox
+            // 
+            this.CarpetasCompartidasListaBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CarpetasCompartidasListaBox.FormattingEnabled = true;
+            this.CarpetasCompartidasListaBox.ItemHeight = 24;
+            this.CarpetasCompartidasListaBox.Location = new System.Drawing.Point(72, 438);
+            this.CarpetasCompartidasListaBox.Name = "CarpetasCompartidasListaBox";
+            this.CarpetasCompartidasListaBox.Size = new System.Drawing.Size(710, 148);
+            this.CarpetasCompartidasListaBox.TabIndex = 11;
+            // 
+            // Infolabel
+            // 
+            this.Infolabel.AutoSize = true;
+            this.Infolabel.Location = new System.Drawing.Point(67, 133);
+            this.Infolabel.Name = "Infolabel";
+            this.Infolabel.Size = new System.Drawing.Size(717, 29);
+            this.Infolabel.TabIndex = 12;
+            this.Infolabel.Text = "Este Programa vas a obtener los datos mas relevantes del Equipo";
+            // 
+            // LogoCofinal
+            // 
+            this.LogoCofinal.Image = ((System.Drawing.Image)(resources.GetObject("LogoCofinal.Image")));
+            this.LogoCofinal.Location = new System.Drawing.Point(72, 25);
+            this.LogoCofinal.Name = "LogoCofinal";
+            this.LogoCofinal.Size = new System.Drawing.Size(710, 86);
+            this.LogoCofinal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoCofinal.TabIndex = 13;
+            this.LogoCofinal.TabStop = false;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 629);
+            this.Controls.Add(this.LogoCofinal);
+            this.Controls.Add(this.Infolabel);
+            this.Controls.Add(this.CarpetasCompartidasListaBox);
+            this.Controls.Add(this.NombreComputadorText);
+            this.Controls.Add(this.NombreDominioText);
+            this.Controls.Add(this.IpText);
+            this.Controls.Add(this.NombreUsuarioText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NombreComputadorLabel);
+            this.Controls.Add(this.DominioLabel);
+            this.Controls.Add(this.IpLabel);
+            this.Controls.Add(this.NombreUsuarioLabel);
+            this.Controls.Add(this.ObtenerButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Dashboard";
+            this.Text = "Informacion Usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoCofinal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ObtenerButton;
+        private System.Windows.Forms.Label NombreUsuarioLabel;
+        private System.Windows.Forms.Label IpLabel;
+        private System.Windows.Forms.Label DominioLabel;
+        private System.Windows.Forms.Label NombreComputadorLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NombreUsuarioText;
+        private System.Windows.Forms.TextBox IpText;
+        private System.Windows.Forms.TextBox NombreDominioText;
+        private System.Windows.Forms.TextBox NombreComputadorText;
+        private System.Windows.Forms.ListBox CarpetasCompartidasListaBox;
+        private System.Windows.Forms.Label Infolabel;
+        private System.Windows.Forms.PictureBox LogoCofinal;
+    }
+}
+
---
+++ OwnProjects/WindowsForms/CofinalDesktopApp/InformacionEquipo/Dashboard.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace InformacionEquipo
+{
+    public partial class Dashboard : Form
+    {
+        List<string> carpetasCompartidas = new List<string>();
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void ObtenerButton_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = Environment.UserName;
+            string nombreDominio = Environment.UserDomainName;
+            string nombreComputador = Environment.MachineName;
+
+            NombreUsuarioText.Text = nombreUsuario;
+            NombreDominioText.Text = nombreDominio;
+            NombreComputadorText.Text = nombreComputador;
+            IpText.Text = ObtenerIp();
+
+            ObtenerCarpetasCompartidas();
+            CarpetasCompartidasListaBox.DataSource = carpetasCompartidas;
+
+        }
+
+        private string ObtenerIp()
+        {
+            string output = "";
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress address in hostEntry.AddressList)
+                {
+                    if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        output = address.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener la dirección IP: " + ex.Message);
+            }
+
+            return output;
+        }
+        private void ObtenerCarpetasCompartidas()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c net use";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    string line = e.Data.Trim();
+                    if (line.StartsWith("OK") || line.StartsWith("Usado") || line.StartsWith("The command completed"))
+                    {
+                        return;
+                    }
+                    if (line.Contains("Conectado"))
+                    {
+                        carpetasCompartidas.Add(line);
+                    }
+                }
+            };
+
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine("Error: " + e.Data);
+                }
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+        }
+    }
+}
