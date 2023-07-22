7296fa49cb9a6da2ee2d3305f7b55f5017271095
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-22 11:52:35 -0500

    MVC Person Model

---
+++ Homeworks/MVC/DemoMVCPersonApp/DemoMVCPerson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoMVCPerson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Homeworks/MVC/DemoMVCPersonApp/DemoMVCPerson/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoMVCPerson.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hello { FirstName } { LastName } !";
+        }
+    }
+}
