17abb7c3734504ab5cd88c542a5c4b8559ae621b
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-07-22 11:52:53 -0500

    Overloading Decimal

---
+++ Homeworks/OverLoadExtensionMiniProject/DemoGetInfoApp/DemoGetInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using DemoGetInfo;
+
+Console.WriteLine("In this program, we will create method extensions and use overloading ");
+Console.WriteLine("This program was bulit by Juan Jose Figueroa");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine();
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "What is your name: ?".RequestString();
+person.LastName = "What is your last name: ?".RequestString();
+person.Count = "What is your numeber count: ?".RequestString();
+
+person.Amount = "What is your amount: ?".RequestDecimal(0,20);
+
+
+Console.WriteLine($"Hello! { person.FirstName } { person.LastName }");
+Console.WriteLine($"Count: { person.Count }");
+
+Console.ReadLine();
