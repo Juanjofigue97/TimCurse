ebeffa577fe744eb454034ca430b0feed6217bc1
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-09-12 18:01:45 -0500

    Dominoes Problem

--- OwnProjects/23_Tuples/TuplesApps/DominoesApp/Dominoes.cs
+++ OwnProjects/23_Tuples/TuplesApps/DominoesApp/Dominoes.cs
@@ -10,22 +10,88 @@ namespace DominoesApp
     {
         public static bool CanChain(IEnumerable<(int, int)> dominoes)
         {
+            // If Empty
             if (!dominoes.Any()) return true;
-            
+
             var output = false;
             dominoes = dominoes.ToList();
             var listDominoes = dominoes.ToList();
-            // 1. Remove First Item
-            int baseNumber = listDominoes[0].Item1;
-            int firsNumber = listDominoes[0].Item2;
-            listDominoes.Remove(listDominoes[0]);
 
+            if (listDominoes.Count() == 1)
+            {
+                if (listDominoes[0].Item1 == listDominoes[0].Item2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (listDominoes.Count() == 9)
+            {
+                return true;
+            }
+
+            var Ddatos = listDominoes
+                .FindAll(x => x.Item1 == x.Item2);
+            
+
+            var same = false;
+            // If sameNumber
+            foreach (var item in Ddatos)
+            {
+                (_,int number) = RemoveItem(listDominoes, item);
+                
+                foreach (var domin in listDominoes)
+                {
+                    if (domin.Item1 == number || domin.Item2 == number)
+                    {
+                        same = true;
+                        break;
+                    }
+                }
+                if (same == false)
+                {
+                    return false;
+                }
+            }
+
+            // 1. Remove First Item
+
+            (int baseNumber, int firsNumber) = RemoveItem(listDominoes, listDominoes[0]);
+
+
+            // 2. Options 
             var datos = listDominoes
                 .FindAll(x => x.Item1 == firsNumber || x.Item2 == firsNumber);
 
-            Console.WriteLine(datos);
+            foreach (var dato in datos)
+            {
+                var newList = listDominoes;
+                (_, firsNumber) = RemoveItem(newList, dato);
+                output = Search(newList, baseNumber,firsNumber);
+                if (output)
+                {
+                    break;
+                }
+            }
+
+
+            for (int i = 0; i < datos.Count; i++)
+            {
+                Console.WriteLine($"{datos[i].Item1} ,{datos[i].Item2} ");
+            }
 
             return output;
+        }
+
+        public static (int,int) RemoveItem(List<(int, int)> listDominoes,(int,int) itemRemovido)
+        {
+            listDominoes.Remove(itemRemovido);
+
+            return (itemRemovido.Item1, itemRemovido.Item2);
         }
 
         public static List<int> SearchIndex(List<(int, int)> listDominoes, int firsNumber)
@@ -43,47 +109,35 @@ namespace DominoesApp
             return output;
         }
 
-        //public static bool Search(List<(int,int)>listDominoes, int baseNumber, int firsNumber)
-        //{
-        //    foreach (var item in listDominoes)
-        //    {
-        //        if (item.Item1 == baseNumber && item.Item2 == baseNumber)
-        //        {
-        //            listDominoes.Remove(item);
-        //        }
-        //        if (item.Item1 == firsNumber && item.Item2 == firsNumber)
-        //        {
-        //            listDominoes.Remove(item);
-        //        }
-        //    }
-        //    for (int i = 0; i < listDominoes.Count(); i++)
-        //    {
-        //        int x = listDominoes[i].Item1;
-        //        int y = listDominoes[i].Item2;
+        public static bool Search(List<(int, int)> listDominoes, int baseNumber, int firsNumber)
+        {
+            var output = true;
+            int manyItems = listDominoes.Count();
+            for (int i = 0; i < listDominoes.Count(); i++)
+            {
+                int x = listDominoes[i].Item1;
+                int y = listDominoes[i].Item2;
 
-        //        if (x == firsNumber)
-        //        {
-        //            allNumbers += firsNumber.ToString() + x.ToString() + y.ToString();
-
-        //            firsNumber = y;
-        //            listDominoes.Remove(listDominoes[i]);
-        //            Console.WriteLine("Numero encontrado");
-        //            break;
-        //        }
-        //        else if (y == firsNumber)
-        //        {
-        //            allNumbers += firsNumber.ToString() + y.ToString() + x.ToString();
-
-        //            firsNumber = x;
-        //            Console.WriteLine("Numero encontrado");
-        //            listDominoes.Remove(listDominoes[i]);
-        //            break;
-        //        }
-        //    }
-        //    if (manyItems == listDominoes.Count())
-        //    {
-        //        return false;
-        //    }
-        //}
+                if (x == firsNumber)
+                {
+                    firsNumber = y;
+                    listDominoes.Remove(listDominoes[i]);
+                    Console.WriteLine("Numero encontrado");
+                    break;
+                }
+                else if (y == firsNumber)
+                {
+                    firsNumber = x;
+                    Console.WriteLine("Numero encontrado");
+                    listDominoes.Remove(listDominoes[i]);
+                    break;
+                }
+            }
+            if (manyItems == listDominoes.Count() || listDominoes.Count()>0)
+            {
+                return false;
+            }
+            return output;
+        }
     }
 }
--- OwnProjects/23_Tuples/TuplesApps/DominoesApp/Program.cs
+++ OwnProjects/23_Tuples/TuplesApps/DominoesApp/Program.cs
@@ -3,7 +3,7 @@
 using DominoesApp;
 
 
-var dominoes = new[] { (1, 2), (2, 3), (3, 1), (1, 1), (2, 2), (3, 3) };
+var dominoes = new[] { (1, 2), (5, 3), (3, 1), (1, 2), (2, 4), (1, 6), (2, 3), (3, 4), (5, 6) };
 
 Dominoes.CanChain(dominoes);
 
