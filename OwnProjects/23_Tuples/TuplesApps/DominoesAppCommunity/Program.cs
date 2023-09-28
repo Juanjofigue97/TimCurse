c943b89338f851fb3f85fec61d013575dfc8bc0f
Juan Jose Figueroa Navarro <Juan.Figueroa@cofinal.com.co>
2023-09-28 11:20:43 -0500

    Tuples Exercises

---
+++ OwnProjects/23_Tuples/TuplesApps/DominoesAppCommunity/Program.cs
@@ -0,0 +1,5 @@
+﻿using static DominoesAppCommunity.DominoesAppCommunity;
+
+var dominoes = new[] { (1, 2), (5, 3), (3, 1), (1, 2), (2, 4), (1, 6), (2, 3), (3, 4), (5, 6) };
+
+DominoesCommunity.CanChain(dominoes);
---
+++ OwnProjects/23_Tuples/TuplesApps/PythagoreanTripletApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using PythagoreanTripletApp;
+
+int targetsum = 90;
+var listnumbers = PythagoreanTriplet.TripletsWithSum(targetsum);
+
+foreach (var item in listnumbers)
+{
+    Console.WriteLine($"{item.a},{item.b},{item.c}");
+}
+
+Console.ReadLine();
