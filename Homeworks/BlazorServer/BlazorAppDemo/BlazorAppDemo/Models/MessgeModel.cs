e1ad49958fe45b1dd88fc9d2834213e356080512
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2023-10-09 10:07:00 -0500

    Blazzor Server App

---
+++ Homeworks/BlazorServer/BlazorAppDemo/BlazorAppDemo/Models/MessgeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppDemo.Models;
+
+
+public class MessgeModel
+{
+    [Required]
+    [StringLength(10,MinimumLength = 5)]
+    public string Message { get; set; }
+}
---
+++ Homeworks/BlazorServer/BlazorAppDemo/BlazorPersonApp/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPersonApp.Models;
+
+public class PersonModel
+{
+    [Required]
+    [StringLength(20, MinimumLength = 5)]
+    public string FirstName { get; set; }
+    [Required]
+    [StringLength(20, MinimumLength = 5)]
+    public string LastName { get; set; }
+
+}
