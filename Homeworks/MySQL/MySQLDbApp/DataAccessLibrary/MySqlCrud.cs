034b12cfec65fb61068e09d85c149d00ce637dbe
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2023-10-17 08:17:11 -0500

    MySQL Exercises

---
+++ Homeworks/MySQL/MySQLDbApp/DataAccessLibrary/MySqlCrud.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLibrary.Models;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class MySqlCrud
+    {
+        private readonly string _connectionString;
+        private MySqlDataAccess db = new MySqlDataAccess();
+
+        public MySqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<PersonModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from People";
+
+            return db.LoadData<PersonModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public void SavePerson(PersonModel person)
+        {
+            string sql = "insert into People (firstname,lastname) VALUES (@firstname,@lastname)";
+            
+            
+            db.SaveData(sql, new
+            {
+                person.FirstName,person.LastName,
+            },_connectionString);
+        }
+    }
+}
---
+++ Homeworks/MySQL/MySQLDbApp/MySQLDb/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+
+MySqlCrud sql = new MySqlCrud(GetConnectionString());
+
+
+//ReadAllContacts(sql);
+
+PersonModel person = new()
+{
+    FirstName = "Camilo",
+    LastName = "Rodriguez"
+};
+
+CreatePerson(sql, person);
+
+
+static void CreatePerson(MySqlCrud sql, PersonModel person)
+{
+    try
+    {
+        sql.SavePerson(person);
+        Console.WriteLine("Person save");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+}
+static void ReadAllContacts(MySqlCrud sql)
+{
+    var rows = sql.GetAllContacts();
+
+    foreach (var row in rows)
+    {
+        Console.WriteLine($"{row.Id}: {row.FirstName} {row.LastName}");
+    }
+}
+
+Console.ReadLine();
+static string GetConnectionString(string connectionStringName = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName);
+
+    return output;
+}
