a3113fb8d294a6dda9321b8219991634eafd67f6
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2023-12-11 13:38:40 -0500

    Dependecy Injection

---
+++ TimCoreyFiles/DependencyInjection/BlazorServerDependecyInjectionDemo/BlazorServerDependecyInjection/Data/Demo.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerDependecyInjection.Data;
+
+public class Demo : IDemo, ILocalDemo
+{
+    public DateTime StartupTime { get; set; }
+
+    public Demo()
+    {
+        StartupTime = DateTime.Now;
+    }
+}
