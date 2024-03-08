f53c86410c20f8e752f5189a3f58e5a1bf69d7be
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2024-03-08 15:06:45 -0500

    Add types of set configurations

--- Homeworks/Appsettings/Basic/BlazorDemoApp/BlazorDemo/Program.cs
+++ Homeworks/Appsettings/Basic/BlazorDemoApp/BlazorDemo/Program.cs
@@ -14,7 +14,30 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.Configure<EmailSettingsOptions>(builder.Configuration.GetSection("EmailSettings"));
 
+var env = builder.Environment.EnvironmentName;
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true);
+
+var memCollection = new Dictionary<string, string>
+{
+    { "MainSetting:SubSetting", "Sub setting from dictionary" }
+};
+
+
+if (env == "Development")
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddCommandLine(args);
+builder.Configuration.AddJsonFile("custom.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddXmlFile("custom.xml", optional: true, reloadOnChange:true) ;
+builder.Configuration.AddIniFile("custom.ini", optional: true, reloadOnChange: true);
+builder.Configuration.AddInMemoryCollection(memCollection);
+//builder.Services.AddOptions<MyOptions>().BindConfiguration("MyConfig");
+
+//builder.Services.AddHostedService<MyWorker>();
 
 builder.WebHost.UseWebRoot("wwwroot").UseStaticWebAssets();
 
