143942d72a92dfe91b2ddb8c108a80a2ec007a71
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2024-03-26 11:43:47 -0500

    Blazor Server Course

---
+++ OwnProjects/46_BlazorServerCourse/ValidationDemo/BlazorApp/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Invalid FirstName length")]
+        public string FirstName { get; set; }
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "Invalid LastName length")]
+        public string LastName { get; set; }
+
+        [Range(1, 125,ErrorMessage = "You need to enter a valid age")]
+        public int Age { get; set; }
+        
+        [Range(typeof(bool),"true","true",ErrorMessage = "You need to give concent to continue.")]
+        public bool GaveConcent { get; set; }
+    }
+}
