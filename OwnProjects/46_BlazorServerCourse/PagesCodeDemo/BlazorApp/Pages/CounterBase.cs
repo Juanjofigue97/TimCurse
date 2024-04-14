243162e9dae17b886e76cefce98e6bc4d8e12ee8
FIGUEROA NAVARRO JUAN JOSE <juanjo.figue97@udenar.edu.co>
2024-04-13 22:12:18 -0500

    Blazor Server Course

---
+++ OwnProjects/46_BlazorServerCourse/PagesCodeDemo/BlazorApp/Pages/CounterBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+
+        [Inject]
+        public ILogger<CounterBase> Log { get; set; }
+        protected void IncrementCount()
+        {
+            currentCount += 2;
+            Log.LogInformation("The new value is{CounterValue}", currentCount);
+        }
+    }
+}
---
+++ OwnProjects/46_BlazorServerCourse/PagesCodeDemo/BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+        }
+    }
+}
